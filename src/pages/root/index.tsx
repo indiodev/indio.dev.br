b98bc7169d05da5808ac5d83f9b37c0b8905d7ce
jhollyfer <jhollyfer.fr@gmail.com>
2024-05-11 23:57:43 -0500

    fix: avatar hero

--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -8,13 +8,6 @@ export function Root(): React.ReactElement {
 	return (
 		<main className="flex flex-1 h-full flex-col items-center justify-center md:gap-10">
 			<div className="flex w-full flex-col items-center justify-center">
-				{/* <img
-					src="/avatar.svg"
-					alt="avatar"
-					className="w-80 h-80"
-					loading="lazy"
-				/> */}
-
 				<Avatar.Hero className="w-80 h-80" />
 
 				<h1 className="font-extrabold text-center text-4xl md:text-5xl text-green-default">
